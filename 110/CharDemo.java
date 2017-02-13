ad49f46cfc2027089ea5affe5c9859b72c713007
Steve Delgado <stevedelgado93@gmail.com>
2017-02-13 01:10:35 -0800

    Comments have been added, fixed indentation, and fixed output.

--- 110/CharDemo.java
+++ 110/CharDemo.java
@@ -4,20 +4,21 @@ public class CharDemo{
 	public static void main (String[]args){
 		char x= 'a';
 		int y= (int)'a';
-		System.out.println("Char x = "+x);
-		System.out.println("int y = "+y);
-		
-		
-	
-		
+		//Provide an example of a character and a cast of an integer
+		//a casts to ascii number.
+		System.out.println("Char a = "+x);
+		System.out.println("int  a = "+y);
+
 		String s="this is a string 123 456";
-		System.out.print("s= "+"\""+s+"\"");
-		
+		//need to use escape character \ to be able to put quotes inside quotes.
+		System.out.print("s= "+"\""+s+"\"\n");
+		//array t has string s that has been split to elements based on space character.
 		String [] t= s.split(" ");
+		//Prints out array t to show how it has been split.
 		for(int i=0; i<t.length;i++){
-		System.out.println("t["+i+"]= "+t[i]);	
+			System.out.println("t["+i+"]= "+t[i]);
 		}
 		int z = Integer.parseInt(t[4])+Integer.parseInt(t[5]);
-		System.out.println(z);
+		System.out.println("t[4]+t[3]= "+z);
 	}
 }
