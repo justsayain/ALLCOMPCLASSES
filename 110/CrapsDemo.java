ee77bed11606559a578c83fd2e6199c75fe51243
Steve Delgado <stevedelgado93@gmail.com>
2017-02-13 01:26:18 -0800

    Simple Craps game should be fixed and runing.

--- 110/CrapsDemo.java
+++ 110/CrapsDemo.java
@@ -1,15 +1,16 @@
 import java.util.*;
 
+//Simple Craps game
 public class CrapsDemo {
 	public static int roll(){
 		Random r= new Random();
 		return r.nextInt(6)+r.nextInt(6) +2;
 	}
-	
+
 	public static boolean craps(){
 		int roll1=roll();
 		//System.out.print(roll1+ " ");
-		
+
 		if(roll1==7 || roll1==11){
 			//System.out.println("you won!");
 			return true;
@@ -38,7 +39,10 @@ public class CrapsDemo {
 		System.out.print("how many games would you like to play? ");
 		int games= input.nextInt();
 		for(int i=1; i<=games;i++){
-			craps();
+			if(craps())
+				System.out.println("you won!");
+			else
+				System.out.println("you lose :(");
 		}
 	}
 }
