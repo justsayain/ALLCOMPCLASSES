af31b631c71cf5ee01d440bfa1672adf930eea7a
Steve Delgado <stevedelgado93@gmail.com>
2017-02-13 00:12:02 -0800

    Addition class has been updated. Also testing gitignore file

--- 110/Addition.java
+++ 110/Addition.java
@@ -1,16 +1,18 @@
 /* Programmer: Steve Delgado
 Project 1: Part 3 */
+
 import java.util.*;
 public class Addition {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int n1, n2, sum;
+		System.out.println("Comp 110 Project 1, Part 3\nAddition of two number provided by user input.");
 		System.out.print("Enter the first integer: ");
 		n1 = input.nextInt();
 		System.out.print("Enter the second integer: ");
 		n2 = input.nextInt();
+		//Provides the sum of two given integers.
 		sum = n1 + n2;
 		System.out.println("The sum of the values is " + sum);
+	}
 }
-}
-
