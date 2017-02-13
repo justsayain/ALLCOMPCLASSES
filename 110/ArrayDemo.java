3465ad2503729c9f1e9df18340faf741fb93ab14
Steve Delgado <stevedelgado93@gmail.com>
2017-02-13 00:53:32 -0800

    Fixing indentation and modifying output.

--- 110/ArrayDemo.java
+++ 110/ArrayDemo.java
@@ -5,13 +5,13 @@ public class ArrayDemo{
 	//method randfil():
 	//create an array, fill with random numbers, return it
 	public static int[] randfill (int n, int range){
-		
+
 		//create array of size n to be returned
 		int []data=new int[n];
-		
+
 		// fill aray with random numbers from 0 to range -1
 		Random random = new Random();
-		
+
 		for(int i=0;i<data.length;i++){		//data.length means the same as n in int[n]
 			data[i] = random.nextInt(range);
 		}
@@ -20,13 +20,13 @@ public class ArrayDemo{
 
 	//method show();
 	//prints but the contents of an array to the display
-	public static void show(int[]data){		//stub empty skeleton 
+	public static void show(int[]data){		//stub empty skeleton
 		for(int i=0;i<data.length;i++){		//data.length means the same as n in int[n]
 			System.out.print(data[i]+" ");	//same loop in order to print out values of previous method
 		}
 		System.out.println();
 	}
-	
+
 	// method min():
 	// finds the min value in an arry and returns int
 	public static int min(int [] data){
@@ -34,7 +34,7 @@ public class ArrayDemo{
 		for(int i=1; i<data.length;i++){
 			if(data[i]<m)
 			m=data[i];
-		}	
+		}
 		return m;
 	}
 	public static int minindex(int [] data){
@@ -42,10 +42,10 @@ public class ArrayDemo{
 		for(int i=1; i<data.length;i++){
 			if(data[i]<m)
 			m=data[i];
-		}	
+		}
 		return m;
 	}
-	
+
 	//look for a value in an array linear(from left to right)
 	public static boolean linearsearch(int [] data, int x){
 		for(int i=0;i<data.length-1;i++){
@@ -54,16 +54,16 @@ public class ArrayDemo{
 		}
 		return false;//returns false after the loop has looked at all the index and does not match x
 	}
-	
-	//void method because it does not need to return 
+
+	//void method because it does not need to return
 	public static void findnreplace(int []data, int x, int y){
 		for(int i=0; i<data.length; i++){
 			if(x==data[i])
 				data[i]=y;
 		}
 	}
-	
-	
+
+
 	// method max():
 	// finds the max value in an arry and returns int
 	public static int max(int [] data){
@@ -71,16 +71,17 @@ public class ArrayDemo{
 	}
 	// method to call swap
 	public static void swap(int x,int y){
+		System.out.println("swap method is being called	.");
 		int temp=x;
 		x=y;
 		y=temp;
-		System.out.print("x= "+x+", y= "+y);
+		System.out.print("a= "+x+", b= "+y);
 	}
 	// swap a number in the index of an array
 	public static void swap(int[] data, int index1,int index2){
 		int temp= data[index1];
 		data[index1]=data[index2];
-		data[index2]=temp;		
+		data[index2]=temp;
 	}
 	//factorial
  	public static int factorial(int n){
@@ -90,31 +91,31 @@ public class ArrayDemo{
 		}
 		return y;
 	}
+
 	//factorial using recursive
 	public static int fact2(int n){
 		//use factoral loop
 		if (n==0)
-		return 1;
+			return 1;
 		else
-		return
-		n*fact2(n-1);
+			return n*fact2(n-1);
 	}
-	
-		
+
+
 	public static void main(String[]args){
+		System.out.print("array x[]: ");
 		int[] x = randfill(10,1000);
 		show(x);
 		System.out.println("min(x) = " + min(x));
-		
 		int[]z = {22,19,87,6,35};
 		System.out.print("\narray z[]: ");
 		show(z);
-		System.out.println(3+ ": "+ linearsearch(z,3));
-		System.out.println(22+ ": "+ linearsearch(z,22));
-		
+		System.out.println(3 + ": "+ linearsearch(z,3)); //is 3 in z?
+		System.out.println(22+ ": "+ linearsearch(z,22)); //is 22 in z?
 		int a=1;
 		int b=2;
+		System.out.println("\na = "+a+"\nb = "+b);
 		swap(a,b);
-		System.out.println("f(3)= "+fact2       (3));
+		System.out.println("\n\nf(3)= "+fact2       (3));
 	}
 }
--- 110/ArrayDemo2.java
+++ 110/ArrayDemo2.java
@@ -1,35 +1,42 @@
 import java.util.*;
+
 public class ArrayDemo2{
+
+	//method print given a string and an array of data prints both out.
 	public static void print(String s,int[] data){
 		System.out.print(s);
 		for(int i=0;i<data.length;i++){
-			System.out.print(data[i]+" ");
+			System.out.print(data[i]+" "); //iterate through array and print it out.
 		}
 		System.out.println();
 	}
+
+	//method to shift data 1 element to the left
 	public static void lcs(int[] data){
-		int a=data[0];		
+		int temp = data[0];
 		for(int i=0;i<data.length-1;i++){
 			data[i]=data[i+1];
 		}
-		data[data.length-1]=a;
+		data[data.length-1]=temp;
 	}
+
+	//method to shift element to the right
 	public static void rcs(int[]data){
-		int a = data[data.length-1];
+		int temp = data[data.length-1];
 		for(int i=data.length-2;i>=0;i--){
 			data[i+1]=data[i];
 		}
-		data[0]=a;
+		data[0]=temp;
 	}
-	
-	
+
+
 	public static void main(String[]args){
-	int[]a={3,5,7,8,2};
-	int[]b={3,5,7,8,2};
-	print("before: ",a);
-	lcs(a);
-	print("after: ",a);
-	rcs(b);
-	print("after: ",b);
+		int[]a={3,5,7,8,2};
+		int[]b={3,5,7,8,2};
+		print("before: ",a);
+		lcs(a); //calls left shift method.
+		print("after: ",a);
+		rcs(b); //calls right shift method.
+		print("after: ",b);
 	}
 }
