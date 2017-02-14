d4328c0bf56effb626b2ed661c6ba6cdd706efa2
Steve Delgado <stevedelgado93@gmail.com>
2017-02-13 16:49:08 -0800

    Java project to solve area under curve using horners rule.

--- 110/PolyAreaOne.java
+++ 110/PolyAreaOne.java
@@ -1,47 +1,47 @@
 /* Steve Delgado
-Comp 110/110L 
-Spring 2013 
+Comp 110/110L
+Spring 2013
 Mon/Wed 8-11am
 Project 5: Area under the curve
  */
-   import java.util.*;
-   public class PolyAreaOne{	//Horners Rule
+import java.util.*;
+
+public class PolyAreaOne{
+      //Horners Rule
       public static double evalpoly(double[] c, double x){
          int degree = c.length+1;
          double y=c[0];
          double power =x;
          for(int i=1;i<c.length;i++){
-         
             y += c[i]*power;
             power*=x;
          }
          return y;
       }
-   
+      //evaluate area under the curve.
       public static double evalarea(double []c, double a, double b, int nsteps){ //method to evaluate area using horners rule
          double area=0;
          double s=(b-a)/nsteps;
          for(double i=a;i<b;i+=s){
             double y= evalpoly(c,i);
-            area += y*s;  
-         }		
-         return area;		
+            area += y*s;
+         }
+         return area;
       }
-   	
-      public static double [] createintegral(double[] c){	//method to create the array/integral of a given polynomial
+   	  //creates integral equation of polynomial.
+      public static double [] createintegral(double[] c){
          int n=c.length;
          int m=n+1;
          double []d = new double [m];
          int i = c.length-1;
          while(i>=0){
             d[i+1]=c[i]/(i+1);
-            i--;		
+            i--;
          }
          d[0]=0;
-         return d;
-      	
+         return d;  //return equation as an array.
       }
-   
+
       public static void main(String[]args){
          Scanner input = new Scanner(System.in);	//create variables
          double [] c;
@@ -51,21 +51,19 @@ Project 5: Area under the curve
          System.out.print("Enter Degree of Polynomial: ");	//user inputs degree;power of polynomial
          n= input.nextInt();
          c= new double[n+1];
-         
+         //iterates to obtain polynomial coefficients.
          for (int i=c.length-1;i>=0;i--){
             System.out.print("Please Enter Coefficient "+i+": "); //for loop to put in Coefficients for polynomial
             c[i] = input.nextDouble();
          }
-      	
-      	
-         
+
          System.out.print("Enter an x you want to begin with: "); //Start x
          a = input.nextDouble();
          System.out.print("Enter an x where you want to stop: "); //Stop x
          b = input.nextDouble();
          System.out.print("Enter step(how many times you want to move horizontally): "); //Enter steps from start x to stop x
          nsteps = input.nextInt();
-         double s = (b-a)/nsteps;   
+         double s = (b-a)/nsteps;
          double []d=createintegral(c);	//creates a new array for the integral
          String f="Integral f(x) = ";
          for(int i=d.length-1;i>=0;i--){
