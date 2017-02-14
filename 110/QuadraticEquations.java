bee80af2b5490e993e7c8e6bcda3bd633a4c1e8e
Steve Delgado <stevedelgado93@gmail.com>
2017-02-13 17:05:51 -0800

    part 2 of Mortgage program. This program simply evaluates quadratic equations.:

--- 110/QuadraticEquations.java
+++ 110/QuadraticEquations.java
@@ -1,15 +1,19 @@
 /* Programmer: Steve Delgado
 Comp 110/110L Spring 2013
 Project 2: Part 2
-Mortgage Interest */
+Mortgage Interest
+Java program that evaluates quadratic equation.
+x = (-b+-sqrt(b^2-4ac))/2a
+*/
 
 import java.util.*;
 public class QuadraticEquations{
 	public static void main(String[]args){
+
 		Scanner input=new Scanner(System.in);
 		//create variables
 		double a,b,c,r1,r2;
-		
+
 		//obtain inputs
 		System.out.print("Enter coefficient a: ");
 		a=input.nextDouble();
@@ -17,12 +21,12 @@ public class QuadraticEquations{
 		b=input.nextDouble();
 		System.out.print("Enter coefficient c: ");
 		c=input.nextDouble();
-		
+
 		//compute results
 		r1=(-b+Math.sqrt(b*b-4*a*c))/(2*a);
 		r2=(-b-Math.sqrt(b*b-4*a*c))/(2*a);
 		//output results
 		System.out.println("The first root is: "+r1);
 		System.out.println("The second root is: "+r2);
-}
+	}
 }
