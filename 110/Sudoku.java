6b81723fab08789bd7d16aa56301ba207aed7cc2
Steve Delgado <stevedelgado93@gmail.com>
2017-02-13 17:40:48 -0800

    Sudoku java missing given testfile from professor.

--- 110/Sudoku.java
+++ 110/Sudoku.java
@@ -1,14 +1,13 @@
-   import java.util.*;
-   import java.io.*;
-
 //Comp 110 Spring 2013
 //Steve Delgado
 //Sudoku Project
+   import java.util.*;
+   import java.io.*;
 
    public class Sudoku{
-   
+
    //intialize rows and columns constant(does not change)
-   
+
       public static final int rows=9;
       public static final int cols=9;
       public static int [][] board= new int[rows][cols];
@@ -16,15 +15,15 @@
    //show()
    //print the contents of the board to the display
    //------------------------------------------------
-      public static void show(){	
+      public static void show(){
          System.out.println("  123 456 789\n");//header for columns
-         for(int i =0;i<rows;i++){	
+         for(int i =0;i<rows;i++){
             System.out.print(i+1+" ");// row #'s
             for(int j=0;j<cols;j++){
                if(board[i][j]==0)
                   System.out.print("_");
                else System.out.print(board[i][j]);//executes for columns
-            
+
                if(j==2)
                   System.out.print("|");
                if(j==5)
@@ -33,21 +32,16 @@
                   System.out.print("\n  ===|===|===");
                if(i==5&&j==8)
                   System.out.print("\n  ===|===|===");
-            
-            
-            
-            
-            
             }
             System.out.println();//executes for rows
          }
       }
-   
+
    //------------------------------------------------
    //load()
    //load the contents of a clue file from the file system
    //------------------------------------------------
-   
+
       public static void load(String filename) {
          Scanner fin= null;
          try{fin = new Scanner(new FileReader(filename));}
@@ -63,24 +57,27 @@
          }
          System.out.println();
       }
-   	
+
       public static void setValue(int r, int c, int value) {
-         board[r][c]=value;  
-      }   
+         board[r][c]=value;
+      }
+
       public static boolean canAddToRow(int r, int value) {
          for (int c=0; c<cols; c++) {
-            if (value==board[r][c]) 
-               return false;
-         }
-         return true;
-      } 
-      public static boolean canAddToColumn(int c, int value) {
-         for (int r=0; r<rows; r++) {
-            if (value==board[r][c]) 
+            if (value==board[r][c])
                return false;
          }
          return true;
       }
+
+      public static boolean canAddToColumn(int c, int value) {
+         for (int r=0; r<rows; r++) {
+            if (value==board[r][c])
+               return false;
+         }
+         return true;
+      }
+
       public static boolean canAddToSubgrid(int r, int c, int value) {
          int sgr=(r/3)*3;
          int sgc=(c/3)*3;
@@ -91,32 +88,34 @@
             }
          }
          return true;
-      }	
+      }
+
       public static boolean isComplete() {
          for (int r=0;r<9; r++){
             for(int value=1;value<=9;value++){
                if(canAddToRow(r,value))
                   System.out.println("Sudoku puzzle not complete");
                return false;
-            } 
+            }
          }
          System.out.println("Sudoku puzzle is complete");
          return true;
       }
+
       public static void commandline() {
-         Scanner in = new Scanner (System.in);       
+         Scanner in = new Scanner (System.in);
          while(true){
             System.out.print(">");
             String s=in.nextLine();
             String[] t= s.split(" ");
-            String cmd=t[0];                   	
+            String cmd=t[0];
             if (cmd.equals("quit"))
                break;
             else if(cmd.equals("show"))
                show();
             else if(cmd.equals("load"))
                load("testfile.txt");
-            	
+
             else if(cmd.equals("set")){
                int r= Integer.parseInt(t[1]);
                int c= Integer.parseInt(t[2]);
@@ -126,26 +125,26 @@
                boolean rcheck = canAddToRow(r,value);
                boolean ccheck = canAddToColumn(c,value);
                boolean scheck = canAddToSubgrid(r,c,value);
-            
+
                if (rcheck && ccheck  && scheck ) {
-                  setValue(r,c,value);              
+                  setValue(r,c,value);
                }
                else {
                   System.out.println(" number not valid ");
                }
-            }           
+            }
             else if(cmd.equals("complete")){
-               isComplete();             
+               isComplete();
             }
             else
                System.out.println("Input not recognized");
-         }   
+         }
       }
-   
+
    //=======================================
    // main method
    //=========================================
-      public static void main(String[]args){		
-         commandline();		
-      }	
+      public static void main(String[]args){
+         commandline();
+      }
    }
