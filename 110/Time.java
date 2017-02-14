ffdfc4238330db1d2c4999b67da57b92b199467a
Steve Delgado <stevedelgado93@gmail.com>
2017-02-13 18:31:09 -0800

    Display Time

--- 110/Time.java
+++ 110/Time.java
@@ -6,22 +6,25 @@
 public class Time{
 	private int hour;
 	private int minute;
-	
+
+	//reset any time to 0:0
 	public Time(){
-		hour =0;
-		minute=0;
+		hour = 0;
+		minute = 0;
 	}
+
 	public Time(int h, int m){
 		if(h>=0 && h<24)
 			hour=h;
 		else
 			hour =0;
-			
+
 		if(m>=0 && m<60)
 			minute=m;
 		else
 			minute =0;
 	}
+	
 	public String toString(){
 		return hour + ":" + minute;
 	}
--- 110/TimeDriver.java
+++ 110/TimeDriver.java
@@ -1,9 +1,13 @@
+//Program to work with Time.java File
+//Simple Driver that contatins main method.
 public class TimeDriver{
+
 	public static void main(String[]args){
+
 		Time t= new Time();
-		
+
 		System.out.println(t.toString());
-		Time u = new Time(32,59);
+		Time u = new Time(22,59);
 		System.out.println(u.toString());
 	}
 }
