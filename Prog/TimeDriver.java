acb86c1bb6a7987bcacf634d2ffe1414f7469e32
Steve Delgado <stevedelgado93@gmail.com>
2017-02-16 01:43:19 -0800

    got rid of duplicate files from 110

--- Prog/TimeDriver.java
+++
@@ -1,9 +0,0 @@
-public class TimeDriver{
-	public static void main(String[]args){
-		Time t= new Time();
-		
-		System.out.println(t.toString());
-		Time u = new Time(32,59);
-		System.out.println(u.toString());
-	}
-}
